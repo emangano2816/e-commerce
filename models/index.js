befe33baa740999d4ea18ef86b6d46438902345a
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-07-20 16:18:04 -0400

    Modified associations to include foreign key references.

--- models/index.js
+++ models/index.js
@@ -6,19 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 //will add category_id to Product
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product,  {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'ProductTag',
+  through: 'product_tag',
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'ProductTag',
+  through: 'product_tag',
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
