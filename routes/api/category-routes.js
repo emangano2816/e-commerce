ee10f3e73f26016dd2673d800afe5b040f48cc08
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-07-20 16:49:46 -0400

    Added GET category by ID route.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,23 +4,33 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  try{
-    // find all categories
-    // be sure to include its associated Products
+  try {
+    // find all categories; include associated Products
     const catData = await Category.findAll({
       include: [{ model: Product }],
-  });
+    });
     res.status(200).json(catData);
   } catch(err) {
     res.status(500).json(err);
-  }
- 
-  
+    };
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    // find one category by its `id` value; include associated Products
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    //if no category id found, return error message
+    if (!catData) {
+      res.status(400).json({message: 'No category foudn with that id.'});
+      return;
+    }
+
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
