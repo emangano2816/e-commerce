05d3a6698375bca138ede9fabb8f0a8ae6f55e3f
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-07-21 14:48:52 -0400

    Added POST Tag route: adds Tag and updates ProductTag table if new associations are included.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,9 +34,37 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+//POST (i.e., create) Tag Rout
+//req.body needs form:
+//   {
+//      "tag_name": "",
+//      "productIds":[]
+//    }
+router.post('/', async (req, res) => {
+  try {
+    // create a new tag
+    const tag = await Tag.create(req.body);
+    //if there's tags with product, need to create pairings to bulk create in the ProductTag model
+    if (req.body.productIds.length) {
+      const tagProductIdArr = req.body.productIds.map((product_id) => {
+        return {
+          tag_id: tag.id,
+          product_id,
+        };
+      });
+      const tagProductIds = await ProductTag.bulkCreate(tagProductIdArr);
+      res.status(200).json(tagProductIds);
+    };
+    //if tag has no product, just respond
+    res.status(200).json(tag)
+  }
+  catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
+
+
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
