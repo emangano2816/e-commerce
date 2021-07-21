f623d8ae87dbbecc9a7ea13edf324be5afa973a5
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-07-21 14:14:32 -0400

    Added GET Tag route: returns all Tags with associated Products.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,18 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+//GET Tags Route
+router.get('/', async (req, res) => {
+  try {
+    // find all tags; include associated Product data
+    const tagData = await Tag.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(tagData);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
