e50fa3c785515d94302a951ca521c25c0e074ada
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-07-21 06:43:02 -0400

    Modified code: added additional comments, beefed up JSON response back to user for POST and PUT.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
+//GET Categories
 router.get('/', async (req, res) => {
   try {
     // find all categories; include associated Products
@@ -15,36 +15,38 @@ router.get('/', async (req, res) => {
     };
 });
 
+//GET Category by ID
 router.get('/:id', async (req, res) => {
   try {
     // find one category by its `id` value; include associated Products
     const catData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
-    //if no category id found, return error message
+    //if no category id found, return error message; otherwise, return category
     if (!catData) {
       res.status(400).json({message: 'No category found with that id.'});
       return;
     }
-
     res.status(200).json(catData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+//POST (ADD new) Category
 router.post('/', async (req, res) => {
   try {
     // create a new category
     const catData = await Category.create({
       category_name: req.body.category_name,
     });
-    res.status(200).json(catData);
+    res.status(200).json([{'message': 'Category added successfully.'}, {id: catData.id, category_name: catData.category_name}]);
   } catch (err) {
     res.status(400).json(err)
   };
 });
 
+//PUT (Update) Category name
 router.put('/:id', async (req, res) => {
   try {
     // update a category by its `id` value
@@ -53,11 +55,11 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
-
+    //if category ID doesn't exist return error message; otherwise, return 
     if (!catData) {
       res.status(400).json({'message': 'No user with that id!'})
     }
-    res.status(200).json(req.body);
+    res.status(200).json([{'message': 'Category updated successfully.'}, {id: req.body.id, category_name: req.body.category_name}]);
   } catch (err) {
     res.status(400).json(err);
   } 
